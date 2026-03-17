9353ea0d783aa775a97f6ec703b656f8e20f5017
Ryotaro Kotaka <225878868+ryokotaka@users.noreply.github.com>
2026-03-17 18:43:27 +0900

    Time: 71 ms (16.54%) | Memory: 28.6 MB (63.78%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                temp = prices[r] - prices[l]
+                maxP = max(maxP, temp)
+            else:
+                l = r
+            
+            r += 1
+        return maxP
